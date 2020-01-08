9a74c644f62fdbae3348ad61bab800fdf04a328a
gafiyatovzz <54131452+gafiyatovzz@users.noreply.github.com>
2020-01-08 13:29:52 +0300

    update

---
+++ Hospital/js/script.js
@@ -0,0 +1,6 @@
+
+const cash = document.querySelector('.column-two__contrast').addEventListener('click', () => {
+      document.body.parentNode.classList.toggle('contrast');
+  })
+
+  console.log(localStorage.cash);
